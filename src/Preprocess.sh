b7f0463acc73aa56a2045f4c84e8d33f1d967f44
Tapiwa <mazaruratapiwa03@gmail.com>
2025-08-09 22:07:03 +0000

    removed gres

--- src/Preprocess.sh
+++ src/Preprocess.sh
@@ -6,7 +6,6 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16      # Using 16 cores (2x E5-2680 have 16 cores total)
 #SBATCH --mem=32G              # Matches the 32GB system RAM per node
-#SBATCH --gres=gpu:2           # Request 2 GPUs per node
 #SBATCH --time=1-00:00:00      # 1 day max runtime (adjust as needed)
 
 # Create log directory
