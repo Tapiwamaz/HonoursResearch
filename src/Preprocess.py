f0cab01f8f84e7e378e429e21761fc73a47a5581
Tapiwa <mazaruratapiwa03@gmail.com>
2025-08-31 01:48:53 +0200

    10e-6

--- src/Preprocess.py
+++ src/Preprocess.py
@@ -49,7 +49,7 @@ X = binned / tic
 # Min-max normalization to [0, 1] range per pixel
 X_min = X.min(axis=1, keepdims=True)
 X_max = X.max(axis=1, keepdims=True)
-X = (X - X_min) / (X_max - X_min + 1e-8)  # Adding a small epsilon to avoid division by zero
+X = (X - X_min) / (X_max - X_min + 1e-6)  # Adding a small epsilon to avoid division by zero
 
 X = X.astype(np.float16)
 print(f"Matrix created!")
