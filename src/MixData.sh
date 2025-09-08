e1dc71ee168c9beaae5b93b912ba6988d447c947
Siyabonga <Siyabonga>
2025-09-08 15:39:09 +0200

    Add scripts for mixing and preprocessing data for Autoencoder training

---
+++ src/MixData.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=MixData
+#SBATCH --output=MixData.log
+#SBATCH --error=MixData_err.log
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --partition=biggpu
+#SBATCH --time=1-00:00:00
+
+INPUT_ONE="../Data/Cancer/hiv-150-1500_x.npy"
+INPUT_TWO="../Data/Cancer/cancer-150-1500_x.npy"
+OUTPUT_DIR="../Data/Pretrain"
+MIXED_NAME="hiv_cancer_150-1500"
+
+mkdir -p "$OUTPUT_DIR"
+
+START_TIME=$(date)
+echo "Mixing data started at: $START_TIME"
+
+python MixingData.py \
+    --inputOne "$INPUT_ONE" \
+    --inputTwo "$INPUT_TWO" \
+    --output "$OUTPUT_DIR" \
+    --name "$MIXED_NAME"
+
+END_TIME=$(date)
+echo "Mixing data finished at: $END_TIME"
+echo "Done!"
---
+++ src/MixingData.py
@@ -0,0 +1,39 @@
+import numpy as np
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description="MIX DATA for pretraining")
+parser.add_argument("--inputOne", required=True, help="Path to the input prerpocessed npy file.")
+parser.add_argument("--inputTwo", required=True, help="Path to the input prerpocessed npy file.")
+
+parser.add_argument("--output", required=True, help="Directory to save the result.")
+parser.add_argument("--name", required=True, help="Name to save output")
+
+
+args = parser.parse_args()
+
+def mix_npy_files(file1_path, file2_path, output_path=None):
+    """
+    Load two .npy files, combine them, shuffle the data, and optionally save.
+    
+    Args:
+        file1_path (str): Path to first .npy file
+        file2_path (str): Path to second .npy file
+        output_path (str, optional): Path to save the mixed data
+    
+    Returns:
+        numpy.ndarray: Combined and shuffled data
+    """
+    data1 = np.load(file1_path)
+    data2 = np.load(file2_path)
+    combined_data = np.concatenate([data1, data2], axis=0)
+    np.random.shuffle(combined_data)
+    
+    if output_path:
+        np.save(output_path, combined_data)
+        print(f"File save to {output_path}")
+    
+
+# Example usage:
+output_path = os.path.join(args.output, f"{args.name}.npy")
+mix_npy_files(file1_path=args.inputOne, file2_path=args.inputOne, output_path=output_path)
