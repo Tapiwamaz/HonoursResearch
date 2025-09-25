5dd836b0c79c8fd4c20bde606f5f52ad9ab6adb9
Tapiwa <mazaruratapiwa03@gmail.com>
2025-09-25 21:12:52 +0200

    Update Jupyter notebooks to use Python 3.12.2 and add OpenH5 script for data processing; remove PlotsH5 script.

---
+++ src/OpenH5.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --job-name=H5
+#SBATCH --output=H5_%j.log
+#SBATCH --error=H5_err_%j.log
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16      # Using more cores for biggpu            
+#SBATCH --partition=bigbatch 
+#SBATCH --time=3-00:00:00      # 3 days max runtime 
+
+INPUT_FILE="../../mass_spec_data/Cancer biopsy/5 June/5 June tumour test 2_1-327482_SN0p0_profile.h5"
+OUTPUT_DIR="../Results/H5"
+NAME="cancer"
+
+
+
+mkdir -p "$OUTPUT_DIR"
+
+if [ ! -f "$INPUT_FILE" ]; then
+    echo "Error: Input file not found at $INPUT_FILE"
+    continue
+fi
+
+
+# Run the Python script
+echo "Starting OpenH5.py for $NAME at $(date)"
+echo "Using input file: $INPUT_FILE"
+echo "Output directory: $OUTPUT_DIR"
+echo "SLURM Job ID: $SLURM_JOB_ID"
+
+python OpenH5.py --input "$INPUT_FILE" --output "$OUTPUT_DIR" --name "$NAME"
+
+echo "$NAME completed at $(date)"
+echo "Results saved to $OUTPUT_DIR"
+
+
+echo "Tapedza!!! Mwari Ngaakudzwe!"
--- src/Openh5.py
+++ src/Openh5.py
@@ -3,67 +3,50 @@ import hdf5plugin
 import numpy as np
 import argparse
 import os
+import matplotlib.pyplot as plt
+
+
+def plot_image( file: h5py.File ,sorted_keys: list[str],mz : float ,shape ,output_dir: str,tolerance: float=0.02):
+    img = np.zeros(shape)
+    for index, key in enumerate(sorted_keys):
+        mass_to_charges = file[str(key)]["x"][:]
+        intensities = file[str(key)]["y"][:]
+        mask = np.abs(mass_to_charges - mz) <= tolerance
+        val = np.sum(intensities[mask]) if np.any(mask) else 0
+        
+        row,col = divmod(index,shape[1])
+        img[row,col] = val
+
+    plt.imshow(img, cmap='viridis', origin='lower')
+    plt.colorbar(label='Intensity')
+    plt.title(f"Ion Image for m/z = {mz}")
+    plt.xlabel("X intensity")
+    plt.ylabel("Y intensity")
+
+    output_path = os.path.join(output_dir, f"image_{mz}.png")
+    plt.savefig(output_path)
+    print(f"Plot saved to {output_path}") 
 
 parser = argparse.ArgumentParser(description="Generate ion image plot.")
 parser.add_argument("--input", required=True, help="Path to the input HDF5 file.")
-parser.add_argument("--coordinates", required=True, help="Path to the coordinates file.")
 parser.add_argument("--output", required=True, help="Directory to save the plot.")
+parser.add_argument("--name", required=True, help="Name of plots.")
+
+
 args = parser.parse_args()
 
 # Load data
 f = h5py.File(args.input, 'r')
-coordinates = np.load(args.coordinates)
-print(f"Number of coords:",len(coordinates))
+print(f"File loaded")
+sorted_keys = sorted(list(f.keys()))
 
-my_spectra = []
-keys = list(f.keys())
-print(f"Number of keys:",len(keys))
-for index in range(len(keys)):
-    if index < len(coordinates):
-        key = keys[index]
-        my_spectra.append([f.get(key)["x"][:],f.get(key)["y"][:],coordinates[index]])
-    
-  
-
-print("Done adding to array!")    
-
-
-import numpy as np
-import matplotlib.pyplot as plt
-# Choose the m/z you want to plot  and tolerance
-target_mz = 271
+target_mzs = [120,150,200,250,271]
 tolerance = 0.02
+width = 400
+height = 400
 
-# Get image dimensions
-all_coords = [coord for _, _, coord in my_spectra]
-xs, ys = zip(*all_coords)
-width = max(xs) + 1
-height = max(ys) + 1
-print(width,height)
+output_path = f"{args.output}{args.name}"
+for mz in target_mzs:
+    plot_image(file=f,sorted_keys=sorted_keys,shape=(width,height),output_dir=output_path,mz=mz)
 
-
-
-# Create empty image
-ion_image = np.zeros((height, width))
-
-# Fill in the ion image with intensities for target m/z
-times = 0 
-for mzs, intensities, (x, y) in my_spectra:
-    # Get mask of indices where mz is within target window
-    mz_mask = (mzs >= target_mz - tolerance) & (mzs <= target_mz + tolerance)
-    if np.any(mz_mask):
-        times+=1
-        ion_intensity = np.mean(intensities[mz_mask])
-        ion_image[y, x] = ion_intensity
-print(f"Points found", times)
-
-plt.imshow(ion_image, cmap='viridis', origin='lower')
-plt.colorbar(label='Ion Intensity')
-plt.title(f"Ion Image for m/z = {target_mz}")
-plt.xlabel("X intensity")
-plt.ylabel("Y intensity")
-# plt.tight_layout()
-
-output_path = os.path.join(args.output, "ion_image_cancer.png")
-plt.savefig(output_path)
-print(f"Plot saved to {output_path}")
+print("Done")
