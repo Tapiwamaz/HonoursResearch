09ef8925f823dedabf0fc5c65b27fe751771fedd
Tapiwa <mazaruratapiwa03@gmail.com>
2025-09-26 13:00:17 +0200

    Kmeans on cancer

--- src/kmeans.sh
+++ src/kmeans.sh
@@ -8,11 +8,11 @@
 #SBATCH --partition=bigbatch
 #SBATCH --time=3-00:00:00      # 3 days max runtime 
 
-INPUT_FILE="../Data/HIV/hiv-150-1500_x.npy"
+INPUT_FILE="../Data/Pretrain/cancer_150_1500_h5_x.npy"
 OUTPUT_DIR="../Results/kmeans"
-COORDS="../Data/HIV/hiv-150-1500_coords.npy"
+COORDS="../Data/Pretrain/cancer_150_1500_h5_x.npy_coords.npy"
 ENCODER="../Models/AE/encoder_250_dropout_wmse.keras"
-JOB_NAME="hiv_wmse"
+JOB_NAME="cance_h5_wmse"
 K_CLUSTERS=3
 
 
