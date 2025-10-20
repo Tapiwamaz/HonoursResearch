9c7b2106ed7f2464219056af1d50750862983a6e
Tapiwa <mazaruratapiwa03@gmail.com>
2025-10-20 23:00:54 +0200

    Done

--- Notebooks/centroids.py
+++ Notebooks/centroids.py
@@ -1,16 +1,18 @@
 import numpy as np
 import tensorflow as tf
 
-centroids = np.load("../kmeans/hiv-h5_centroids_k3.npy")
+centroids = np.load("../Data/HIV/hiv-150-1500_x.npy")
 # centroids = np.array([np.round(mz, 4) for mz in centroids])
 # mzs = np.load("../Data/HIV/mzs.npy")
 
-decoder = tf.keras.models.load_model("../Models/Decoder/encoder_h5_wmse_200_decoder.keras")
+decoder = tf.keras.models.load_model("../Models/Decoder/decoder.keras")
 out = decoder.predict(centroids)
 del decoder
 del centroids
-print(f"Shape: {out.shape}")
+print(f"0: {out[0]}")
+print(f"1: {out[1]}")
+print(f"2: {out[2]}")
 
-np.savetxt("../c1y.txt", out[0], fmt="%.4f")
-np.savetxt("../c2y.txt", out[1], fmt="%.4f")
-np.savetxt("../c3y.txt", out[2], fmt="%.4f")
+np.savetxt("../c11y.txt", out[0], fmt="%.4f")
+np.savetxt("../c22y.txt", out[1], fmt="%.4f")
+np.savetxt("../c33y.txt", out[2], fmt="%.4f")
