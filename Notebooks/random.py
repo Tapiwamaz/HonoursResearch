f317d4b954a50198d46778e5c2a9da7bc7b3c9ae
Tapiwa <mazaruratapiwa03@gmail.com>
2025-10-20 22:29:02 +0200

    random

---
+++ Notebooks/random.py
@@ -0,0 +1,16 @@
+import numpy as np
+from tensorflow.keras.models import  load_model
+
+centroids = np.load("../kmeans/hiv-h5_centroids_k3.npy")
+# centroids = np.array([np.round(mz, 4) for mz in centroids])
+# mzs = np.load("../Data/HIV/mzs.npy")
+
+decoder = load_model("../Models/Decoder/encoder_h5_wmse_200_decoder.keras")
+out = decoder.predict(centroids)
+del decoder
+del centroids
+print(f"Shape: {out.shape}")
+
+np.savetxt("../c1y.txt", out[0], fmt="%.4f")
+np.savetxt("../c2y.txt", out[1], fmt="%.4f")
+np.savetxt("../c3y.txt", out[2], fmt="%.4f")
