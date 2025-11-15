cf211ab10e5f03badff746d73477cb7ffa6cae33
Tapiwa <mazaruratapiwa03@gmail.com>
2025-11-15 14:27:39 +0200

    Wrong one

--- src/3d.sh
+++ src/3d.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=H5
-#SBATCH --output=H5.log
-#SBATCH --error=H5_err.log
+#SBATCH --output=3D.log
+#SBATCH --error=3D_err.log
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16      # Using more cores for biggpu            
-#SBATCH --partition=bigbatch
+#SBATCH --partition=biggpu
 #SBATCH --time=3-00:00:00      # 3 days max runtime 
 
-INPUT_FILE="../../mass_spec_data/"
+INPUT_FILE="../../mass_spec_data/Cancer biopsy/5 June/5 June tumour test 2_1-327482_SN0p0_profile.h5"
 OUTPUT_DIR="../3D/"
 NAME="cancer"
 
