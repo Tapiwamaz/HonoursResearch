dd42db7da6a5b710b0d189b82c05e976fbbff372
Tapiwa <mazaruratapiwa03@gmail.com>
2025-11-15 13:55:55 +0200

    Do 3d

---
+++ src/3D.py
@@ -0,0 +1,73 @@
+import matplotlib.pyplot as plt
+import h5py
+import hdf5plugin
+import numpy as np
+import argparse
+import os
+import matplotlib.pyplot as plt
+import math
+
+
+def prepare_data(file: h5py.File, name :str , output_dir: str,sorted_keys: list[str]) -> None:
+    my_spectra = []
+    max_mz = -float('inf')
+    min_mz = float('inf')
+    for idx, key in enumerate(sorted_keys):
+        mzs = file[str(key)]["x"][:]
+        max_mz = math.ceil(max(max(mzs),max_mz))
+        min_mz = math.floor(min(min(mzs),min_mz))
+        intensities =file[str(key)]["y"][:]
+        my_spectra.append([mzs, intensities])
+
+    print(f'Range of mz values:',(min_mz,max_mz))
+
+
+    common_mzs = np.arange(min_mz,max_mz,1)
+    binned = np.zeros((len(my_spectra), len(common_mzs)), dtype=np.float32)
+
+    for i, (mzs, intensities) in enumerate(my_spectra):
+        indices = np.digitize(mzs, common_mzs) - 1
+        for k, val in zip(indices, intensities):
+            if 0 <= k < binned.shape[1]:
+                binned[i, k] += val
+
+
+    # tic = binned.sum(axis=1, keepdims=True)
+    # X = binned / (tic + 1e-10)
+
+    # del binned
+
+    # X_min = X.min(axis=1, keepdims=True)
+    # X_max = X.max(axis=1, keepdims=True)
+    # X = (X - X_min) / (X_max - X_min + 1e-6)  # Adding a small epsilon to avoid division by zero
+
+
+    X = X.astype(np.float32)
+    print(f"Matrix created!")
+    print(f"Matrix has dimensions of", X.shape)
+
+    data_name = f"3d-{name}_x.npy"
+    output = os.path.join(output_dir, data_name)
+    np.save(output, X)
+    data_name = f"3d-{name}_mzs.npy"
+    output = os.path.join(output_dir, data_name)
+    np.save(output, common_mzs)
+
+
+
+parser = argparse.ArgumentParser(description="Generate ion image plot.")
+parser.add_argument("--input", required=True, help="Path to the input HDF5 file.")
+parser.add_argument("--output", required=True, help="Directory to save the plot.")
+parser.add_argument("--name", required=True, help="Name of plots.")
+
+
+args = parser.parse_args()
+
+# Load data
+f = h5py.File(args.input, 'r')
+print(f"File loaded")
+sorted_keys = sorted([int(key) for key in f.keys()])
+
+
+
+prepare_data(sorted_keys=sorted_keys,output_dir=args.output,name=args.name,file=f)
---
+++ src/3d.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --job-name=H5
+#SBATCH --output=H5.log
+#SBATCH --error=H5_err.log
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16      # Using more cores for biggpu            
+#SBATCH --partition=bigbatch
+#SBATCH --time=3-00:00:00      # 3 days max runtime 
+
+INPUT_FILE="../../mass_spec_data/"
+OUTPUT_DIR="../3D/"
+NAME="cancer"
+
+
+
+mkdir -p "$OUTPUT_DIR"
+
+if [ ! -f "$INPUT_FILE" ]; then
+    echo "Error: Input file not found at $INPUT_FILE"
+    continue
+fi
+
+
+# Run the Python script
+echo "Starting OpenH5.py for $NAME at $(date)"
+echo "Using input file: $INPUT_FILE"
+echo "Output directory: $OUTPUT_DIR"
+echo "SLURM Job ID: $SLURM_JOB_ID"
+
+python Openh5.py --input "$INPUT_FILE" --output "$OUTPUT_DIR" --name "$NAME"
+
+echo "$NAME completed at $(date)"
+echo "Results saved to $OUTPUT_DIR"
+
+
+echo "Tapedza!!! Mwari Ngaakudzwe!"
