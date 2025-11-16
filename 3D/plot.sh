f4ebe51072068f21ae7d12c252763917e25d4d99
Tapiwa <mazaruratapiwa03@gmail.com>
2025-11-16 15:23:35 +0200

    Did hiv and cancer with t=50

--- 3D/plot.sh
+++ 3D/plot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=H5
-#SBATCH --output=3D_plot.log
+#SBATCH --output=3D_plot_%j.log
 #SBATCH --error=3D_plot_err.log
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -8,10 +8,10 @@
 #SBATCH --partition=bigbatch
 #SBATCH --time=3-00:00:00      # 3 days max runtime 
 
-INPUT_FILE="../../mass_spec_data/Cancer biopsy/5 June/5 June tumour test 2_1-327482_SN0p0_profile.h5"
-# INPUT_FILE="../../mass_spec_data/HIV/3 June/3 June PHRU FFPE test 1_1-115501_SN0p0_profile.h5"
-OUTPUT_DIR="./cancer"
-NAME="cancer"
+#INPUT_FILE="../../mass_spec_data/Cancer biopsy/5 June/5 June tumour test 2_1-327482_SN0p0_profile.h5"
+INPUT_FILE="../../mass_spec_data/HIV/3 June/3 June PHRU FFPE test 1_1-115501_SN0p0_profile.h5"
+OUTPUT_DIR="./hiv"
+NAME="hiv"
 
 mkdir -p "$OUTPUT_DIR"
 
