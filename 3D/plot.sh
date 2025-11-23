db1ff640b6e9ff00e26253e2c9be7f69a7143fb5
Tapiwa <mazaruratapiwa03@gmail.com>
2025-11-23 13:49:09 +0200

    added coreeect path

--- 3D/plot.sh
+++ 3D/plot.sh
@@ -8,7 +8,7 @@
 #SBATCH --partition=bigbatch
 #SBATCH --time=3-00:00:00      # 3 days max runtime 
 
-INPUT_FILE="../../mass_spec_data/HIV/3 June/3 June PHRU FFPE test 1_1-115501_SN0p0_profile.h5"
+INPUT_FILE="../../mass_spec_data/HIV/3 June/3 June PHRU FFPE test 1_1-115501_SN0p0_1-56679_SN1p0_centroid.imzml"
 mkdir -p "$OUTPUT_DIR"
 
 
